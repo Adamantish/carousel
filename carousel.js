032a25db58e44d4973a09613a25abd86f8ee0da9
Adamantish <sonsequence@hotmail.com>
2015-10-29 11:15:27 +0000

    Dots have separate pages when more than 12. Needs tidying.

--- carousel.js
+++ carousel.js
@@ -1,22 +1,31 @@
 
+dotsLimit = 12
 images = document.getElementById('carousel-images');
-
-window.imageWidth = 679;
-window.currImage = 0
+dots = document.getElementById("jump-dots")
+lightGrey ="rgb(217, 217, 217)"
+orange = "rgb(254, 169, 52)"
+  
 window.initialOffset = 0
+window.imageWidthPx = 680;
+window.dotWidthEm = 3
+window.currImage = 0
+
 window.currImageLeft = initialOffset
+window.currDotPageStart
+window.currDotPageLeft
 
 function setImageUlWidth () { 
-  var images = document.getElementById('carousel-images')
   window.numImages = images.childElementCount
-  var widthString = (imageWidth - 1) * numImages + "px"
+
+  // These lines currently don't seem to work.
+  var widthString = (imageWidthPx - 1) * numImages + "px;"
   images.style.width = widthString
-  images.setAttribute("width" , widthString )
+  images.setAttribute("style" , "width:" + widthString )
 }
 
-function moveImage(goBack) {
+function shiftImage(goBack) {
   
-  sign = !!goBack ? -1 : 1;
+  var sign = !!goBack ? -1 : 1;
 
   var i = currImage + sign
   i = i >= numImages ? 0 : i;
@@ -26,8 +35,28 @@ function moveImage(goBack) {
   }
 }
 
+function shiftJumpDotPage(goBack) {
+
+  var sign = !!goBack ? -1 : 1;
+
+  var shiftDistance = dotsLimit - 2
+  var i = currDotPageStart + (shiftDistance * sign)
+
+  if(i >= 0) {
+    shiftToDotPageStart(i)
+  }
+
+  currDotPageStart = i
+  // var dotInPage = index % dotsLimit
+  // if(dotInPage >= (dotsLimit - 1)){
+
+  // }
+
+}
+
 function goToImage(index) {
-  currImageLeft = initialOffset - (index * imageWidth)
+  currImageLeft = -1 * index * imageWidthPx
+  // Why does this next line know what images is?
   images.setAttribute('style','left:' + currImageLeft + 'px');
   switchDotColour(index, true)
   if(currImage !== index) {
@@ -37,9 +66,15 @@ function goToImage(index) {
   currImage = index
 }
 
+function shiftToDotPageStart(index) {
+  // this is too similar to above function
+  currDotPageLeft = -1 * index * dotWidthEm
+  var dots = document.getElementById('jump-dots')
+  dots.setAttribute('style','left:' + currDotPageLeft + 'em');
+
+}
+
 function switchDotColour(index, selected) {
-  var lightGrey ="rgb(217, 217, 217)"
-  var orange = "rgb(254, 169, 52)"
 
   var colour = !!selected ? orange : lightGrey;
   var dot = document.getElementById("dot" + index)
@@ -48,8 +83,6 @@ function switchDotColour(index, selected) {
 
 function createJumpDots() {
 
-  var dots = document.getElementById("jump-dots")
-  
   for(var i = 0; i < numImages; i++){
    
     var dot = document.createElement("div") 
@@ -58,8 +91,23 @@ function createJumpDots() {
     dot.setAttribute("onclick","goToImage(" + i + ")")
     dots.appendChild(dot)
   }
+  setJumpDotContainerWidth()
+  setJumpDotsWidth()
+
+  window.currDotPageStart = 0
+
 }
 
+function setJumpDotContainerWidth (dotsLimitOverride) {
+  var numDisplayDots = numImages > dotsLimit ? dotsLimit : numImages;
+  var dotsContainer = dots.parentElement
+  dotsContainer.setAttribute("style", "width: " + numDisplayDots * 3 + "em;")
+}
+
+function setJumpDotsWidth () {
+  // This blats the left property currently
+  dots.setAttribute("style", "width: " + numImages * 3 + "em;")
+}
 setImageUlWidth()
 createJumpDots()
 goToImage(0)
